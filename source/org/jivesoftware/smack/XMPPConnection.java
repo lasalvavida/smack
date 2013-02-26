0f9b3d47d8e7d7622ebf16ae2dabf2e6f88aca67
Florian Schmaus <fschmaus@gmail.com>
2013-02-26 11:16:02 +0100

    SMACK-417 If both PacketReader and PacketWriter fail at the same time, connectionClosedonError() is called two times
    Refactored notifyConnectionError() and notifyReconnection() from
    PacketReader to XMPPConnection. Made PacketReader.done and
    PacketWriter.done volatile. Prevent duplicate
    connectionClosedonError() calls by making the method synchronzied and
    protected with an enter guard:
    
    if (packetReader.done && packetWriter.done) return;

--- source/org/jivesoftware/smack/XMPPConnection.java
+++ source/org/jivesoftware/smack/XMPPConnection.java
@@ -627,7 +627,7 @@ public class XMPPConnection extends Connection {
                 }
             }
             else if (!wasAuthenticated) {
-                packetReader.notifyReconnection();
+                notifyReconnection();
             }
 
         }
@@ -739,7 +739,7 @@ public class XMPPConnection extends Connection {
     void startTLSReceived(boolean required) {
         if (required && config.getSecurityMode() ==
                 ConnectionConfiguration.SecurityMode.disabled) {
-            packetReader.notifyConnectionError(new IllegalStateException(
+            notifyConnectionError(new IllegalStateException(
                     "TLS required by server but not allowed by connection configuration"));
             return;
         }
@@ -753,7 +753,7 @@ public class XMPPConnection extends Connection {
             writer.flush();
         }
         catch (IOException e) {
-            packetReader.notifyConnectionError(e);
+            notifyConnectionError(e);
         }
     }
 
@@ -943,7 +943,7 @@ public class XMPPConnection extends Connection {
             writer.flush();
         }
         catch (IOException e) {
-            packetReader.notifyConnectionError(e);
+            notifyConnectionError(e);
         }
     }
 
@@ -1007,7 +1007,7 @@ public class XMPPConnection extends Connection {
                     login(config.getUsername(), config.getPassword(),
                             config.getResource());
                 }
-                packetReader.notifyReconnection();
+                notifyReconnection();
             }
             catch (XMPPException e) {
                 e.printStackTrace();
@@ -1023,6 +1023,53 @@ public class XMPPConnection extends Connection {
     private void setWasAuthenticated(boolean wasAuthenticated) {
         if (!this.wasAuthenticated) {
             this.wasAuthenticated = wasAuthenticated;
+        }
+    }
+
+    /**
+     * Sends out a notification that there was an error with the connection
+     * and closes the connection.
+     *
+     * @param e the exception that causes the connection close event.
+     */
+    synchronized void notifyConnectionError(Exception e) {
+        // Listeners were already notified of the exception, return right here.
+        if (packetReader.done && packetWriter.done) return;
+
+        packetReader.done = true;
+        packetWriter.done = true;
+        // Closes the connection temporary. A reconnection is possible
+        shutdown(new Presence(Presence.Type.unavailable));
+        // Print the stack trace to help catch the problem
+        e.printStackTrace();
+        // Notify connection listeners of the error.
+        for (ConnectionListener listener : getConnectionListeners()) {
+            try {
+                listener.connectionClosedOnError(e);
+            }
+            catch (Exception e2) {
+                // Catch and print any exception so we can recover
+                // from a faulty listener
+                e2.printStackTrace();
+            }
+        }
+    }
+    
+
+    /**
+     * Sends a notification indicating that the connection was reconnected successfully.
+     */
+    protected void notifyReconnection() {
+        // Notify connection listeners of the reconnection.
+        for (ConnectionListener listener : getConnectionListeners()) {
+            try {
+                listener.reconnectionSuccessful();
+            }
+            catch (Exception e) {
+                // Catch and print any exception so we can recover
+                // from a faulty listener
+                e.printStackTrace();
+            }
         }
     }
 }
